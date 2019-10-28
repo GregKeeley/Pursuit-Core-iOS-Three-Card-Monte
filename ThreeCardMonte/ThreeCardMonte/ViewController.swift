41b65fcfb62cba805a0fb1baf7c1122ad8bae004
Gregory Keeley <gregkeeley@Gregorys-MacBook-Pro.local>
2019-10-28 17:55:43 -0400

    Updated lab

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    @IBOutlet weak var cardLeft: UIButton!
+    @IBOutlet weak var cardMiddle: UIButton!
+    @IBOutlet weak var cardRight: UIButton!
+    @IBOutlet weak var playerMessage: UILabel!
+    
+    let images = ["cardBackRed.png",
+                  "kingCard.jpg",
+                  "threeCard.png"]
+    
+    
+    @IBAction func cardSelect(_ sender: UIButton) {
+        let cardSelection: (bgColor: UIColor?, winLose: String)
+        switch sender.tag {
+        case 0:
+            cardSelection = (.red, "WRONG")
+        case 1:
+            cardSelection = (.red, "WRONG")
+        case 2:
+            cardSelection = (.green, "Correct!")
+        default:
+            break
+            
+        }
+        view.backgroundColor = cardSelection.bgColor
+        playerMessage.text = cardSelection.winLose
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
-
